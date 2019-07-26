cc22677e247548319f07fc7ff08af1c64696a539
Alan Chu <alan_chu@yahoo.com>
2019-07-26 17:02:03 +1000

    Button event generates random number

--- 06-clicker/src/App.js
+++ 06-clicker/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Rando from "./Rando";
 import BrokenClick from "./BrokenClick";
-
+import Clicker from "./Clicker";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <BrokenClick />
+      <Clicker />
     </div>
   );
 }
