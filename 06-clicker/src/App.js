3300ea09183f38f0b94718e9ab2c6bed9bbe085d
Alan Chu <alan_chu@yahoo.com>
2019-07-26 15:46:49 +1000

    Setup Interval Timer of 1s interval at the constructor to demonstrate how to set the state : number

--- 06-clicker/src/App.js
+++ 06-clicker/src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Rando />
+      <Rando maxNum={7} />
     </div>
   );
 }
--- 06-clicker/src/Rando.js
+++ 06-clicker/src/Rando.js
@@ -4,9 +4,16 @@ class Rando extends Component {
   constructor(props) {
     super(props);
     this.state = { num: 0 };
+    this.makeTimer();
+  }
+  makeTimer() {
+    setInterval(() => {
+      let rand = Math.floor(Math.random() * this.props.maxNum);
+      this.setState({ num: rand });
+    }, 1000);
   }
   render() {
-    return <h1>Random number</h1>;
+    return <h1>{this.state.num}</h1>;
   }
 }
 
