56a42f956c6413774233617c53d8a64d86a1a46e
Alan Chu <alan_chu@yahoo.com>
2019-07-29 16:03:56 +1000

    Setting up the structure for a API calling application

---
+++ grid-07-form-inputs/src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <label>Image Search</label>
+          <input
+            className="field"
+            type="text"
+            value={this.state.term}
+            onChange={e => this.setState({ term: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
