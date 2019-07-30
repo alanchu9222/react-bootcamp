ae941bd885f04b89f4dcb401c5101c4abec14e40
Alan Chu <alan_chu@yahoo.com>
2019-07-30 10:55:39 +1000

    Use fat arrow function for callback function to avoid the problem with undefined 'this' when the function is being called

--- grid-07-form-inputs/src/components/App.js
+++ grid-07-form-inputs/src/components/App.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -11,12 +12,14 @@ class App extends React.Component {
           "Client-ID 8c968c529c285c7fa3b0a67aa3c6dd190d9cf46fb4965211960ad3e5c3e1ee4b"
       }
     });
+    this.setState({ images: response.data.results });
     console.log(response.data.results);
-  }
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} results!
       </div>
     );
   }
