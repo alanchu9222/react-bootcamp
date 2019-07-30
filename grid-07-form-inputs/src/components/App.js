6b1ccb2e3ecc42b77373c20a6974e636f11a70fa
Alan Chu <alan_chu@yahoo.com>
2019-07-30 10:42:09 +1000

    Updated the search component to use alternative syntax for asynchronous operation: async, await

--- grid-07-form-inputs/src/components/App.js
+++ grid-07-form-inputs/src/components/App.js
@@ -3,20 +3,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    console.log(term);
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID 8c968c529c285c7fa3b0a67aa3c6dd190d9cf46fb4965211960ad3e5c3e1ee4b"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
-  };
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 8c968c529c285c7fa3b0a67aa3c6dd190d9cf46fb4965211960ad3e5c3e1ee4b"
+      }
+    });
+    console.log(response.data.results);
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
