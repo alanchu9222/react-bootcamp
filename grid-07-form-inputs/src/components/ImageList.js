3975d980e774ac9eaa9ae719fba0a9027093d5b4
Alan Chu <alan_chu@yahoo.com>
2019-07-30 13:21:08 +1000

    Dynamic spans using React Ref

--- grid-07-form-inputs/src/components/ImageList.js
+++ grid-07-form-inputs/src/components/ImageList.js
@@ -1,13 +1,13 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
-class ImageList extends React.Component {
-  render() {
-    const results = this.props.images;
-    const imageUrls = results.map(result => (
-      <img alt={result.description} key={result.id} src={result.urls.regular} />
-    ));
-    return imageUrls;
-  }
-}
+const ImageList = props => {
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+
+  return <div className="image-list">{images}</div>;
+};
 
 export default ImageList;
