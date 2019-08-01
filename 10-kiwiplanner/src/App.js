eb287be44b69b0806f3131860083c044163451f0
Alan Chu <alan_chu@yahoo.com>
2019-08-01 16:40:52 +1000

    Plumbing for passing data between the user interface and the rendering components

--- 10-kiwiplanner/src/App.js
+++ 10-kiwiplanner/src/App.js
@@ -6,12 +6,31 @@ import TravelPlan from "./components/TravelPlan";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    current_user: "",
+    current_state: "logged-out",
+    view_mode: 0
+  };
+  //
+  // This call back function gets the current state from the back end
+  // when triggered by the user and is used to inform the rendering
+  // components on how to present the information
+  //
+  updateAuthState = newState => {
+    this.setState({
+      current_user: newState.user,
+      current_state: newState.state
+    });
+  };
   render() {
     return (
       <div>
-        <NavBar />
-        <SlideMenu />
-        <TravelPlan />
+        <NavBar setState={this.updateAuthState} />
+        <TravelPlan
+          user={this.state.current_user}
+          viewmode={this.state.view_mode}
+          authState={this.state.current_state}
+        />
       </div>
     );
   }
