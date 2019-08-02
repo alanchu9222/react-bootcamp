29e5b9f1375834d382e0c92bbf5b6dd4942e2792
Alan Chu <alan_chu@yahoo.com>
2019-08-02 15:48:07 +1000

    Firebase auth setup and conditional rendering of navbar and side menu

--- 10-kiwiplanner/src/App.js
+++ 10-kiwiplanner/src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import SlideMenu from "./components/SlideMenu";
 import TravelPlan from "./components/TravelPlan";
+import { DB_CONFIG } from "./config/config";
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    app.initializeApp(DB_CONFIG);
+    this.db = app.firestore();
+    this.auth = app.auth();
+  }
+
   state = {
     current_user: "",
-    current_state: "logged-out",
-    view_mode: 0
+    isLoggedIn: true,
+    view_mode: 0,
+    auth: null,
+    db: null
   };
+
   //
   // This call back function gets the current state from the back end
   // when triggered by the user and is used to inform the rendering
@@ -19,13 +32,18 @@ class App extends React.Component {
   updateAuthState = newState => {
     this.setState({
       current_user: newState.user,
-      current_state: newState.state
+      logged_in: newState.logged_in
     });
   };
   render() {
     return (
       <div>
-        <NavBar setState={this.updateAuthState} />
+        <NavBar
+          setState={this.updateAuthState}
+          isLoggedIn={this.state.isLoggedIn}
+          auth={this.auth}
+        />
+
         <TravelPlan
           user={this.state.current_user}
           viewmode={this.state.view_mode}
--- 10-kiwiplanner/src/components/NavBar.js
+++ 10-kiwiplanner/src/components/NavBar.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import Auth from "./Auth";
 import M from "materialize-css";
 class NavBar extends Component {
   static defaultProps = {
+    auth: null,
     menuItems: [
       {
         list_id: "0",
@@ -13,7 +15,7 @@ class NavBar extends Component {
       {
         list_id: "1",
         menu_id: "logout",
-        target: "",
+        target: "modal-logout",
         description: "Logout",
         show_when: "logged-in"
       },
@@ -22,7 +24,7 @@ class NavBar extends Component {
         menu_id: "",
         target: "modal-create",
         description: "Create Plan",
-        show_when: "admin"
+        show_when: "logged-in"
       },
       {
         list_id: "3",
@@ -51,6 +53,40 @@ class NavBar extends Component {
     console.log(this.props.menuItems);
     console.log(this.props.menuItems[0].id);
   }
+  renderMenuItem = menuItem => {
+    return (
+      <li key={menuItem.list_id} className="{menuItem.show_when}">
+        <span
+          className="z-depth-0 teal lighten-4 grey-text waves-effect waves-light btn modal-trigger"
+          data-target={menuItem.target}
+          id={menuItem.menu_id}
+        >
+          {menuItem.description}
+        </span>
+      </li>
+    );
+  };
+  renderSideMenuItem = menuItem => {
+    return (
+      <li key={menuItem.list_id}>
+        <span
+          className="z-depth-0 white grey-text waves-effect waves-light btn modal-trigger"
+          data-target={menuItem.target}
+          id={menuItem.menu_id}
+        >
+          {menuItem.description}
+        </span>
+      </li>
+    );
+  };
+
+  currentMenuItems = () => {
+    console.log("Show current login status");
+    console.log(this.props);
+    const mode = this.props.isLoggedIn ? "logged-in" : "logged-out";
+    return this.props.menuItems.filter(a => a.show_when === mode);
+  };
+
   render() {
     return (
       <div>
@@ -61,39 +97,24 @@ class NavBar extends Component {
             </span>
 
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              {this.props.menuItems.map(menuItem => (
-                <li key={menuItem.list_id} className="{menuItem.show_when}">
-                  <span
-                    className="z-depth-0 teal lighten-4 grey-text waves-effect waves-light btn modal-trigger"
-                    data-target={menuItem.target}
-                    id={menuItem.menu_id}
-                  >
-                    {menuItem.description}
-                  </span>
-                </li>
-              ))}
+              {this.currentMenuItems().map(this.renderMenuItem)}
             </ul>
           </div>
         </nav>
+
         {this.props.menuItems.map(menuItem => (
           <div key={menuItem.list_id} id={menuItem.target} className="modal">
+            {/* The auth component is mapped to a user event on the menus */}
+            {/* The "target" is used to determine what to render on the modal */}
             <div className="modal-content">
               <h4>{menuItem.description}</h4>
+              <Auth target={menuItem.target} auth={this.props.auth} />
             </div>
           </div>
         ))}
+
         <ul className="sidenav" id="mobile-nav">
-          {this.props.menuItems.map(menuItem => (
-            <li key={menuItem.list_id}>
-              <span
-                className="z-depth-0 white grey-text waves-effect waves-light btn modal-trigger"
-                data-target={menuItem.target}
-                id={menuItem.menu_id}
-              >
-                {menuItem.description}
-              </span>
-            </li>
-          ))}
+          {this.currentMenuItems().map(this.renderSideMenuItem)}
         </ul>
       </div>
     );
