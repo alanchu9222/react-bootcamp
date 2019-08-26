0fa2df3712da98d5d17585f1e7dca8d7d3488b45
Alan Chu <alan_chu@yahoo.com>
2019-08-26 17:59:09 +1000

    handle form submit event

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -2,16 +2,23 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-  inputChangeHandler = event => {};
+  inputChangeHandler = event => {
+    this.setState({ term: event.target.value });
+  };
+  formSubmitHandler = event => {
+    event.preventDefault();
+    // Remember to pass the value up to the parent via a callback function
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.formSubmitHandler} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
               type="text"
-              onChange={inputChangeHandler}
+              onChange={this.inputChangeHandler}
               value={this.state.term}
             />
           </div>
