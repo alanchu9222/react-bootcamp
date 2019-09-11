3204fa1a549c5002f9cfc7b85f6c57e1bc71b7c0
Alan Chu <alan_chu@yahoo.com>
2019-09-11 17:25:50 +1000

    Added modal to respond to Add button click

--- 13_MERN_SHOPPING_LIST/client/src/App.js
+++ 13_MERN_SHOPPING_LIST/client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import AppNavBar from "./components/AppNavBar";
+import ItemModal from './components/ItemModal';
 import ShoppingList from "./components/ShoppingList";
+import { Container } from 'reactstrap';
 import { Provider } from 'react-redux';
 import "./App.css";
 import store from './store';
@@ -11,7 +13,10 @@ function App() {
     <Provider store={store}>    
     <div className="App">
       <AppNavBar />
+      <Container>
+      <ItemModal />
       <ShoppingList />
+      </Container>
     </div>    
     </Provider>
   );
--- 13_MERN_SHOPPING_LIST/client/src/actions/itemActions.js
+++ 13_MERN_SHOPPING_LIST/client/src/actions/itemActions.js
@@ -5,3 +5,6 @@ export const getItems = () => {
 export const deleteItem = id => {
   return { type: DELETE_ITEM, payload: id };
 };
+export const addItem = item => {
+  return { type: ADD_ITEM, payload: item };
+};
