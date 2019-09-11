a5f16af7b29f3f8e63193fa955043f1f348042d5
Alan Chu <alan_chu@yahoo.com>
2019-09-11 16:36:52 +1000

    Added redux to handle the states for get list items and delete list item

--- 13_MERN_SHOPPING_LIST/client/src/App.js
+++ 13_MERN_SHOPPING_LIST/client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import AppNavBar from "./components/AppNavBar";
 import ShoppingList from "./components/ShoppingList";
-
+import { Provider } from 'react-redux';
 import "./App.css";
+import store from './store';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
+    <Provider store={store}>    
     <div className="App">
       <AppNavBar />
       <ShoppingList />
     </div>    
+    </Provider>
   );
 }
 
