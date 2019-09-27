31765bfc0c9bc46c243afb7c6c906bb1f77f5f3d
Alan Chu <alan_chu@yahoo.com>
2019-09-27 18:35:18 +1000

    Add userid into the auth reducer. Added types for action creators.

--- grider-16-streams/client/src/reducers/authreducer.js
+++ grider-16-streams/client/src/reducers/authreducer.js
@@ -1,20 +1,17 @@
-// import { SIGN_IN, SIGN_OUT } from "../actions/types";
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
 
 const INTIAL_STATE = {
-  isSignedIn: null
-  //   ,
-  //   userId: null
+  isSignedIn: null,
+  userId: null
 };
 
 export default (state = INTIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
-      return { ...state, isSignedIn: true };
-    //   return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
 
-    case "SIGN_OUT":
-      return { ...state, isSignedIn: false };
-    //   return { ...state, isSignedIn: false, userId: null };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
