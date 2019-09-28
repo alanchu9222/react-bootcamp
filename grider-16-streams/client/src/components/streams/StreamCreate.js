aea43e591dce08263f76c9ee61789225e67cbe46
Alan Chu <alan_chu@yahoo.com>
2019-09-28 11:27:32 +1000

    Basic implementation of redux forms - check the correct operation using redux dev tool on chrome

--- grider-16-streams/client/src/components/streams/StreamCreate.js
+++ grider-16-streams/client/src/components/streams/StreamCreate.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+
 //import { connect } from "react-redux";
 // import { createStream } from "../../actions";
 // import StreamForm from "./StreamForm";
@@ -7,17 +9,43 @@ class StreamCreate extends React.Component {
   //   onSubmit = formValues => {
   //     this.props.createStream(formValues);
   //   };
+  renderInput(formProps) {
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
+
+  //   renderInput = ({ input, label, meta }) => {
+  //     const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+  //     return (
+  //       <div className={className}>
+  //         <label>{label}</label>
+  //         <input {...input} autoComplete="off" />
+  //         {this.renderError(meta)}
+  //       </div>
+  //     );
+  //   };
 
   render() {
     return (
-      <div>
-        <h3>Create a Stream</h3>
-        {/* <StreamForm onSubmit={this.onSubmit} /> */}
-      </div>
+      <form>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
     );
   }
 }
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
 
 //export default connect()(StreamCreate);
 //   null
