6d0acdbf2faae4f7622ce6074c35932db59fd5c4
Alan Chu <alan_chu@yahoo.com>
2019-09-30 12:56:33 +1000

    Grider 253- All buttons linked for Show Streams List - Create/Edit/Delete

--- grider-16-streams/client/src/components/streams/StreamList.js
+++ grider-16-streams/client/src/components/streams/StreamList.js
@@ -8,23 +8,23 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-    renderAdmin(stream) {
-      if (stream.userId === this.props.currentUserId) {
-        return (
-          <div className="right floated content">
-            <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
-              Edit
-            </Link>
-            <Link
-              to={`/streams/delete/${stream.id}`}
-              className="ui button negative"
-            >
-              Delete
-            </Link>
-          </div>
-        );
-      }
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
+          </Link>
+        </div>
+      );
     }
+  }
 
   renderList() {
     return this.props.streams.map(stream => {
@@ -43,24 +43,24 @@ class StreamList extends React.Component {
     });
   }
 
-  //   renderCreate() {
-  //     if (this.props.isSignedIn) {
-  //       return (
-  //         <div style={{ textAlign: "right" }}>
-  //           <Link to="/streams/new" className="ui button primary">
-  //             Create Stream
-  //           </Link>
-  //         </div>
-  //       );
-  //     }
-  //   }
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
 
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
-        {/* {this.renderCreate()} */}
+        {this.renderCreate()}
       </div>
     );
   }
@@ -69,16 +69,11 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
-    // isSignedIn: state.auth.isSignedIn
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   };
 };
 export default connect(
   mapStateToProps,
   { fetchStreams }
 )(StreamList);
-
-//export default connect()(StreamList);
-//   mapStateToProps
-//   ,
-//   { fetchStreams }
