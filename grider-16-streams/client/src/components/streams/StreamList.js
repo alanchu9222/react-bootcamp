9231da9c71c90fc32e07ff8522564acda1ba6c41
Alan Chu <alan_chu@yahoo.com>
2019-09-30 12:26:39 +1000

    Grider 250: show list of streams

--- grider-16-streams/client/src/components/streams/StreamList.js
+++ grider-16-streams/client/src/components/streams/StreamList.js
@@ -4,9 +4,9 @@ import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
-  //   componentDidMount() {
-  //     this.props.fetchStreams();
-  //   }
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
   //   renderAdmin(stream) {
   //     if (stream.userId === this.props.currentUserId) {
@@ -26,22 +26,22 @@ class StreamList extends React.Component {
   //     }
   //   }
 
-  //   renderList() {
-  //     return this.props.streams.map(stream => {
-  //       return (
-  //         <div className="item" key={stream.id}>
-  //           {this.renderAdmin(stream)}
-  //           <i className="large middle aligned icon camera" />
-  //           <div className="content">
-  //             <Link to={`/streams/${stream.id}`} className="header">
-  //               {stream.title}
-  //             </Link>
-  //             <div className="description">{stream.description}</div>
-  //           </div>
-  //         </div>
-  //       );
-  //     });
-  //   }
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {/* {this.renderAdmin(stream)} */}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {/* <Link to={`/streams/${stream.id}`} className="header"> */}
+            {stream.title}
+            {/* </Link> */}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
 
   //   renderCreate() {
   //     if (this.props.isSignedIn) {
@@ -58,23 +58,23 @@ class StreamList extends React.Component {
   render() {
     return (
       <div>
-        <h2>List Streams</h2>
-        {/* <div className="ui celled list">{this.renderList()}</div>
-        {this.renderCreate()} */}
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {/* {this.renderCreate()} */}
       </div>
     );
   }
 }
 
-// const mapStateToProps = state => {
-//   return {
-//     streams: Object.values(state.streams)
-//     // currentUserId: state.auth.userId,
-//     // isSignedIn: state.auth.isSignedIn
-//   };
-// };
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams)
+    // currentUserId: state.auth.userId,
+    // isSignedIn: state.auth.isSignedIn
+  };
+};
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStreams }
 )(StreamList);
 
