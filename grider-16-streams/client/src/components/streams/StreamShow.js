a26296853da2eb9f383247ee4467702648800f87
Alan Chu <alan_chu@yahoo.com>
2019-10-04 09:53:01 +1000

    Cleanup the react-bootcamp repo

--- grider-16-streams/client/src/components/streams/StreamShow.js
+++ grider-16-streams/client/src/components/streams/StreamShow.js
@@ -2,39 +2,41 @@ import React from "react";
 import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+const videoStyle = {
+  width: "200px"
+};
 
 class StreamShow extends React.Component {
   constructor(props) {
     super(props);
     this.videoRef = React.createRef();
   }
-
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchStream(id);
-    this.buildPlayer();
+    // this.buildPlayer();
   }
 
-  componentDidUpdate() {
-    this.buildPlayer();
-  }
+  // componentDidUpdate() {
+  //   this.buildPlayer();
+  // }
 
-  componentWillUnmount() {
-    this.player.destroy();
-  }
+  // componentWillUnmount() {
+  //   this.player.destroy();
+  // }
 
-  buildPlayer() {
-    if (this.player || !this.props.stream) {
-      return;
-    }
-    const { id } = this.props.match.params;
-    this.player = flv.createPlayer({
-      type: "flv",
-      url: `http://localhost:8000/live/${id}.flv`
-    });
-    this.player.attachMediaElement(this.videoRef.current);
-    this.player.load();
-  }
+  // buildPlayer() {
+  //   if (this.player || !this.props.stream) {
+  //     return;
+  //   }
+  //   const { id } = this.props.match.params;
+  //   this.player = flv.createPlayer({
+  //     type: "flv",
+  //     url: `http://localhost:8000/live/${id}.flv`
+  //   });
+  //   this.player.attachMediaElement(this.videoRef.current);
+  //   this.player.load();
+  // }
 
   render() {
     if (!this.props.stream) {
@@ -45,8 +47,14 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        Show Streams
-        <video ref={this.videoRef} style={{ width: "100%" }} controls />
+        <h1>Show Streams</h1>
+        {/* <video ref={this.videoRef} style={{ width: "100%" }} controls /> */}
+        <video height="380" controls>
+          <source
+            src="https://github.com/alanchu9222/salsa_collection/raw/master/outside-turn/2019-09-01.mp4"
+            type="video/mp4"
+          />
+        </video>
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
