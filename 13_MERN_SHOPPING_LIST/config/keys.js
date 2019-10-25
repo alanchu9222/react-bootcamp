a2436263615a65e76df6e6684e805c4813bced23
Alan Chu <alan_chu@yahoo.com>
2019-10-25 12:58:39 +1100

    Added image_repo to organise scattered image files

--- 13_MERN_SHOPPING_LIST/config/keys.js
+++ 13_MERN_SHOPPING_LIST/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://alanchu:Pa$$w0rd@cluster0-vgrym.mongodb.net/mern_shopping?retryWrites=true&w=majority' 
-}
+  mongoURI:
+    "mongodb+srv://alanchu:Pa$$w0rd@cluster0-vgrym.mongodb.net/mern_shopping?retryWrites=true&w=majority"
+};
